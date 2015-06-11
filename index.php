87543f2d67cad56492afbcbb809882423b028509
chiricos <edwarddiaz92@gmail.com>
2015-06-11 14:53:00 -0500

    comenzando a maquetar

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>zebra studio</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Drawde"/>
+    <meta name="description" content="zebra studio"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1"/>
+    <!-- Style -->
+    <link rel="stylesheet" href="{{asset('css/normalize.min.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}"/>
+
+    <!-- Style <script src="{{asset('js/prefixfree.min.js')}}"></script>-->
+    <script src="{{asset('js/jquery-1.11.2.min.js')}}"></script>
+</head>
+<body>
+<header>
+    <div>
+        <section>
+            <div>
+                <a>ES</a>
+                <a>EN</a>
+            </div>
+            <div>
+                <a>F</a>
+                <a>T</a>
+                <a>V</a>
+            </div>
+        </section>
+        <figure>
+            <img src="images/logo.png">
+        </figure>
+        <nav>
+            <ul>
+                <li>
+                    <a>Zebra Studio</a>
+                </li>
+                <li>
+                    <a>Portafolio</a>
+                </li>
+                <li>
+                    <a>Servicios</a>
+                </li>
+                <li>
+                    <a>Clientes</a>
+                </li>
+                <li>
+                    <a>Equipo</a>
+                </li>
+                <li>
+                    <a>Contactenos</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+</header>
+<div id="wrapper">
+
+</div>
+<footer>
+    <section>
+        <ul>
+            <li>
+                <a>Zebra Studio</a>
+            </li>
+            <li>
+                <a>Portafolio</a>
+            </li>
+            <li>
+                <a>Servicios</a>
+            </li>
+            <li>
+                <a>Clientes</a>
+            </li>
+            <li>
+                <a>Equipo</a>
+            </li>
+            <li>
+                <a>Contactenos</a>
+            </li>
+        </ul>
+    </section>
+    <article>
+        <p>El diseño debe seducir, educar y, quizás lo más importante, provocar una respuesta emocional.</p>
+        <span>April Gremiman</span>
+    </article>
+    <div>
+
+    </div>
+</footer>
+</body>
+</html>

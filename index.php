a75cf5d22fe25b0d494b763675cb7074578c9ae4
chiricos <edwarddiaz92@gmail.com>
2015-06-12 17:38:24 -0500

    slider completo y en desarrollo las imagenes de los trabajos hechos por zebra

--- index.php
+++ index.php
@@ -8,11 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1"/>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,700,300,600,800,400' rel='stylesheet' type='text/css'>
     <!-- Style -->
+    <link rel="stylesheet" href="animate.min.css">
     <link rel="stylesheet" href="css/normalize.min.css"/>
     <link rel="stylesheet" href="css/style.css"/>
 
     <!-- Style <script src="{{asset('js/prefixfree.min.js')}}"></script>-->
-    <script src="{{asset('js/jquery-1.11.2.min.js')}}"></script>
+    <script src="js/jquery-1.11.2.min.js"></script>
+    <script src="js/slider.js"></script>
+    <script src="js/hover.js"></script>
 </head>
 <body>
 <div class="panel_up"></div>
@@ -67,16 +70,62 @@
 <div id="wrapper">
     <div class="content">
         <section>
-            <figure>
-                <img src="images/slider.png">
+            <figure id="title_slider" >
+                <img id="reel" class="img-slider animated bounceIn" src="images/title_slider1.png" >
+                <img id="reel" class="img-slider animated bounceIn" src="images/title_slider2.png" >
+                <img id="reel" class="img-slider animated bounceIn" src="images/title_slider3.png" >
+                <img id="reel" class="img-slider animated bounceIn" src="images/title_slider4.png" >
             </figure>
+            <div id="slider">
+                <img class="img-slider animated fadeInUp" src="images/slider1.jpg" />
+                <img class="img-slider animated fadeInUp" src="images/slider2.jpg" />
+                <img class="img-slider animated fadeInUp" src="images/slider3.jpg" />
+                <img class="img-slider animated fadeInUp" src="images/slider4.jpg" />
+                <span  class="slider sliderValid" style="  margin-left: 85%;">1</span>
+                <span  class="slider ">2</span>
+                <span  class="slider ">3</span>
+                <span  class="slider ">4</span>
+            </div>
         </section>
     </div>
     <div class="content">
         <section>
-            <figure>
-                <img src="images/slider.png">
-            </figure>
+            <div id="content_works">
+                <div class="content_line">
+                    <div class="content_line1"></div>
+                    <div class="content_line2"></div>
+                </div>
+                <div class="content_all_work">
+                    <section class="works_zebra">
+                        <figure>
+                            <img src="images/tv.jpg">
+                            <img src="images/web.jpg">
+                        </figure>
+                        <figure>
+                            <img src="images/multimedia.jpg">
+                            <img src="images/app.jpg">
+                            <img src="images/fotografia.jpg">
+                        </figure>
+                    </section>
+                    <section class="works_zebra_hover">
+                        <figure>
+                            <div class="hover_figure_one animated "><p>COMPERCIALES <br /> TV / VIDEO</p></div>
+                            <div class="hover_figure_two animated "><p>WEB</p></div>
+                        </figure>
+                        <figure>
+                            <div class="hover_figure_three animated "><p>MULTIMEDIA</p></div>
+                            <div class="hover_figure_four animated "><p>APP</p></div>
+                            <div class="hover_figure_five animated "><p>FOTOGRAFÍA <br />  ILUSTRACIÓN</p></div>
+                        </figure>
+                    </section>
+                </div>
+
+
+            <div class="content_line">
+                <div class="content_line2" style="margin: 30px auto auto"></div>
+                <div class="content_line1"></div>
+            </div>
+            </div>
         </section>
     </div>
 </div>
